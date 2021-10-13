a249f1bf6fcc2f3865837586f40a55cc10f299c8
Krit Sriyanuwatkul <krit.s@acommerce.asia>
2021-10-13 18:57:34 +0700

    init vue & bootstrap

---
+++ main.js
@@ -0,0 +1,9 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
+// import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.min.js'
+
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
