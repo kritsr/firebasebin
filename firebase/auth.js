bc915b5c352e6f179c08930c698e3e20d3cecc8a
Krit Sriyanuwatkul <krit.s@acommerce.asia>
2021-10-14 01:07:11 +0700

    Add basic auth function with ui

--- firebase/auth.js
+++ firebase/auth.js
@@ -2,28 +2,33 @@ import {
     getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,
     setPersistence, browserLocalPersistence, onAuthStateChanged
 } from 'https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js'
+import {ref} from 'https://cdn.jsdelivr.net/npm/vue@3.2.20/dist/vue.esm-browser.js'
 
 const auth = getAuth();
 
-export let user;
+// Make user reactive
+export let userRef = ref();
+function setUser(user) {
+    return userRef.value = user
+}
 
 await setPersistence(auth, browserLocalPersistence); // Persist login data in local storage
 
 // Keep user updated and fetch persisted data when refresh
-onAuthStateChanged(auth, function(_user) {
-    user = _user
+onAuthStateChanged(auth, function(user) {
+    setUser(user)
 })
 
 export { _createUserWithEmailAndPassword as createUserWithEmailAndPassword }
 async function _createUserWithEmailAndPassword(email, password) {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    return user = userCredential.user;
+    return setUser(userCredential.user);
 }
 
 export { _signInWithEmailAndPassword as signInWithEmailAndPassword }
 async function _signInWithEmailAndPassword(email, password) {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    return user = userCredential.user;
+    return setUser(userCredential.user);
 }
 
 export { _signOut as signOut }
--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 import {createApp} from 'https://cdn.jsdelivr.net/npm/vue@3.2.20/dist/vue.esm-browser.js'
 // import {createApp} from 'https://cdn.jsdelivr.net/npm/vue@3.2.20/dist/vue.esm-browser.prod.js'
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, userRef} from './firebase/auth.js'
 
 const app = createApp({
     data() {
-        return {message: 'Hello Vue!!'}
+        return {
+            user: userRef,
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        signOut,
+        signUp,
+        signIn
     }
 }).mount('#app');
+
+function onError(e) {
+    console.error(e.code);
+    console.error(e.message);
+}
+
+async function signUp() {
+    try {
+        await createUserWithEmailAndPassword(this.email, this.password);
+    } catch (e) {
+        onError(e)
+    }
+}
+
+async function signIn() {
+    try {
+        await signInWithEmailAndPassword(this.email, this.password);
+    } catch (e) {
+        onError(e)
+    }
+}
